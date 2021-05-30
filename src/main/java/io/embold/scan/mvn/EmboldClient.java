0ba1facbf432c34433b2043bf93e63904ce26da6
Sudarshan Bhide <sudarshan.bhide@embold.io>
2021-05-30 10:53:35 +0530

    additional logging and improvements

--- src/main/java/io/embold/scan/mvn/EmboldClient.java
+++ src/main/java/io/embold/scan/mvn/EmboldClient.java
@@ -14,13 +14,13 @@ import kong.unirest.Unirest;
  */
 public class EmboldClient {
 
-    private String apiVersion = "v1";
-    private final String GET_REPO = "/api/" + apiVersion + "/repositories";
-    private final String CREATE_REPO = "/api/" + apiVersion + "/repositories";
+    private static final String apiVersion = "v1";
+    private static final String GET_REPO = "/api/" + apiVersion + "/repositories";
+    private static final String CREATE_REPO = "/api/" + apiVersion + "/repositories";
 
-    private String emboldUrl;
-    private String emboldToken;
-    private Log logger;
+    private final String emboldUrl;
+    private final String emboldToken;
+    private final Log logger;
 
     public EmboldClient(Log logger, String emboldUrl, String emboldToken) {
         super();
@@ -31,13 +31,14 @@ public class EmboldClient {
 
     public void getCreateRepo(Repo repo) throws MojoExecutionException {
         boolean repoFound = false;
+        logger.info("Finding existing repo: " + repo);
         HttpResponse<JsonNode> response = Unirest.get(this.emboldUrl + GET_REPO + "/" + repo.getRepoUid())
                 .header("accept", "application/json").header("Content-Type", "application/json")
                 .header("Authorization", "Bearer " + this.emboldToken).asJson();
         if (response.isSuccess()) {
             JsonNode body = response.getBody();
             if (StringUtils.isNotEmpty(body.getObject().getString("repoName"))) {
-                logger.info("Found repo: " + repo.getRepoUid() + ":" + repo.getRepoName());
+                logger.info("Found repo: " + repo);
                 repoFound = true;
             }
         } else {
@@ -50,14 +51,18 @@ public class EmboldClient {
         }
 
         if (!repoFound) {
-            logger.info("Creating: " + repo);
-            response = Unirest.post(this.emboldUrl + CREATE_REPO).header("accept", "application/json")
-                    .header("Content-Type", "application/json").header("Authorization", "Bearer " + this.emboldToken)
+            logger.info("Creating new repo: " + repo);
+            response = Unirest.post(this.emboldUrl + CREATE_REPO)
+                    .header("accept", "application/json")
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + this.emboldToken)
                     .body(repo.jsonString()).asJson();
             if (!response.isSuccess()) {
                 String msg = "Error while creating repo on Embold server: " + errorResponse(response);
                 logger.error(msg);
                 throw new MojoExecutionException(msg);
+            } else {
+                logger.info("Created new repo: " + repo);
             }
         }
     }
@@ -67,11 +72,9 @@ public class EmboldClient {
         String error = "Unknown error";
         if (body.has("error")) {
             JSONObject errObj = body.getJSONObject("error");
-            StringBuilder sb = new StringBuilder();
-            sb.append(errObj.has("code") ? errObj.getInt("code") : -1).append(" | ")
-                    .append(errObj.has("name") ? errObj.getString("name") : "Unknown error").append(" | ")
-                    .append(errObj.has("message") ? errObj.getString("message") : "");
-            error = sb.toString();
+            error = (errObj.has("code") ? errObj.getInt("code") : -1) + " | " +
+                    (errObj.has("name") ? errObj.getString("name") : "Unknown error") + " | " +
+                    (errObj.has("message") ? errObj.getString("message") : "");
         }
         return error;
     }
